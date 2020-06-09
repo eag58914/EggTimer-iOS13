85cc2a3a6de05096268ed12f7f001a3ff1c3aaec
Elco Garcia <garciaelco18@gmail.com>
2020-06-08 21:01:02 -0700

    Timer works with progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,29 +7,55 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    let eggTimes = ["Soft": 5,"Medium":7,"Hard":12]
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progessBar: UIProgressView!
     
-
+    let eggTimes = ["Soft": 3,"Medium":4,"Hard":7]
+    
+  
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    
+    
+    
     @IBAction func hardnessSelector(_ sender: UIButton) {
         
-        let hardness = sender.currentTitle!
-        print(hardness)
         
-        if hardness == "Soft"{
-            print(eggTimes["Soft"]!)
-        }else if hardness == "Medium"{
-            print(eggTimes["Medium"]!)
-        }else{
-            print(eggTimes["Hard"]!)
-        }
+    
+        timer.invalidate()
+        
+        
+        let hardness = sender.currentTitle!
+    
+        totalTime = eggTimes[hardness]!
+        
+        progessBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+      timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil,repeats: true)
     }
+        @objc func updateTimer(){
+            if secondsPassed < totalTime{
+                
+                secondsPassed += 1
+                let percentageProgress = Float(secondsPassed)  / Float(totalTime)
+                
+                progessBar.progress = percentageProgress
+               
+                
+            }else{
+                timer.invalidate()
+                titleLabel.text = "Done"
+            }
+        }
     
-    
-
-    
-    
+   
     
     
 }
