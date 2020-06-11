2a4021389278014ce3b6dc1eaa3f71d8c9bb3e5d
Elco Garcia <garciaelco18@gmail.com>
2020-06-10 17:43:50 -0700

    Sound plays when timer finishes now

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,26 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var progessBar: UIProgressView!
     
     let eggTimes = ["Soft": 3,"Medium":4,"Hard":7]
+    var player: AVAudioPlayer!
     
   
     var timer = Timer()
     var totalTime = 0
     var secondsPassed = 0
-    
+    var soundTitle = "alarm_sound"
     
     
     
     @IBAction func hardnessSelector(_ sender: UIButton) {
-        
-        
-    
         timer.invalidate()
-        
-        
         let hardness = sender.currentTitle!
     
         totalTime = eggTimes[hardness]!
-        
         progessBar.progress = 0.0
         secondsPassed = 0
         titleLabel.text = hardness
@@ -52,8 +47,19 @@ class ViewController: UIViewController {
             }else{
                 timer.invalidate()
                 titleLabel.text = "Done"
+                self.playSound(soundTitle: soundTitle)
             }
         }
+    
+    
+    //this is to play the done timer
+    
+    func playSound(soundTitle:String) {
+        let url = Bundle.main.url(forResource: soundTitle, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
     
    
     
